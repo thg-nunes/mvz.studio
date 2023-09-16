4904f44ad30eb39c2f7990ad07382a28d862b7ea
Thiago Nunes <desenvolvedor.nunes@gmail.com>
2023-09-16 12:13:22 -0300

    chore: adiciona config de cores ao tema do tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,21 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {},
+  content: ['./src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {
+      colors: {
+        black: '#16181E',
+        gray: {
+          100: '#F9F9F9',
+          900: '#21242D',
+        },
+        cyan: {
+          500: '#00B9AE',
+        },
+      },
+    },
+  },
   plugins: [],
 }
 export default config
