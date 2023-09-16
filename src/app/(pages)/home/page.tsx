3b237736fef5062fb203e3dd7bb92eab18cb6791
Thiago Nunes <desenvolvedor.nunes@gmail.com>
2023-09-16 13:42:03 -0300

    feat: iniciando construcao do menu laateral

---
+++ src/app/(pages)/home/page.tsx
@@ -0,0 +1,3 @@
+export default function HomePage() {
+  return <main className="right-0 w-[85%] bg-black"></main>
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Poppins } from 'next/font/google'
+import { LinkComponent } from '@components/link'
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({ subsets: ['latin'], weight: ['400', '600', '700'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -11,8 +12,37 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-BR">
+      <body className={poppins.className}>
+        <div className="flex">
+          <section className="h-screen w-[15%] border border-gray-100/10 bg-gray-900 py-4">
+            <p className="text-center font-bold text-white">MVZ.studio</p>
+            <LinkComponent
+              pathName="Home"
+              linkIcon={
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  data-testid="home-icon"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M2 13.5V7h1v6.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V7h1v6.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5zm11-11V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"
+                  />
+                  <path
+                    fillRule="evenodd"
+                    d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"
+                  />
+                </svg>
+              }
+            />
+          </section>
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
