91de042d1949918934ac4ef5797edab97d32f39f
Thiago Nunes <desenvolvedor.nunes@gmail.com>
2023-09-16 13:38:43 -0300

    feat: continuacao da implementacao do linkComponent

--- src/app/components/link.tsx
+++ src/app/components/link.tsx
@@ -1,19 +1,28 @@
 'use client'
 import Link from 'next/link'
 import { ReactElement } from 'react'
+import { usePathname } from 'next/navigation'
 
 export type LinkProps = {
   linkIcon: ReactElement
   pathName: string
 }
 
+const linkStyle = {
+  active: 'flex items-center gap-2 text-cyan-600 border-r border-cyan-600',
+  default: 'flex items-center gap-2 text-white',
+}
+
 export const LinkComponent = ({ linkIcon, pathName }: LinkProps): JSX.Element => {
   const pathNameToLoawerCase = pathName.toLowerCase()
+  const activePathName = usePathname()
+  const style =
+    activePathName === `/${pathNameToLoawerCase}` ? linkStyle.active : linkStyle.default
 
   return (
-    <section className="flex items-center gap-2 text-white">
+    <Link href={`/${pathNameToLoawerCase}`} className={style}>
       {linkIcon}
-      <Link href={`/${pathNameToLoawerCase}`}>{pathName}</Link>
-    </section>
+      {pathName}
+    </Link>
   )
 }
