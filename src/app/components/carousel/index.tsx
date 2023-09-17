0d650dc373290e0a7c8a7dc7b98dbff207a6ea19
Thiago Nunes <desenvolvedor.nunes@gmail.com>
2023-09-17 12:46:50 -0300

    feat: cria script para o coursel automatico de filmes

---
+++ src/app/components/carousel/index.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import './carouselScriptAnimation'
+
+export type CourselProps = {}
+
+export const Coursel = (): JSX.Element => {
+  return (
+    <div id="carouselContainer" className="relative h-[400px] w-[70%] overflow-hidden">
+      <section id="carousel" className="flex transition-all duration-1000 ease-linear ">
+        <img
+          src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
+          alt="image 1"
+          className="carouselElement h-full w-full object-cover"
+        />
+        <img
+          src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
+          alt="image 2"
+          className="carouselElement h-full w-full object-cover"
+        />
+        <img
+          src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
+          alt="image 3"
+          className="carouselElement h-full w-full object-cover"
+        />
+      </section>
+    </div>
+  )
+}
