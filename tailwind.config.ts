e8dcd83612cbdae56867b8fe74d0e6dff7464f29
Thiago Nunes <desenvolvedor.nunes@gmail.com>
2023-09-18 16:16:14 -0300

    chore: instalacao e configuracao da lib material-tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss'
+import withMT from '@material-tailwind/react/utils/withMT'
 
-const config: Config = {
+const config: Config = withMT({
   content: ['./src/app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
@@ -20,5 +21,5 @@ const config: Config = {
     },
   },
   plugins: [],
-}
+})
 export default config
