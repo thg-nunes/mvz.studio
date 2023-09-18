ed3b010df0019bf1a0cdf1e7f9c759807f596dc7
Thiago Nunes <desenvolvedor.nunes@gmail.com>
2023-09-18 16:21:38 -0300

    chore: corrige nome das .env variables

--- src/utils/movieImage.ts
+++ src/utils/movieImage.ts
@@ -1,3 +1,3 @@
 export const returnsMovieURL = (imageWidth: number, file_path: string): string => {
-  return `${process.env.TMDB_IMAGE_ENDPOINT}/w${imageWidth}${file_path}?api_key=${process.env.TMDB_API_KEY}`
+  return `${process.env.NEXT_PUBLIC_TMDB_IMAGE_ENDPOINT}/w${imageWidth}${file_path}?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}`
 }
