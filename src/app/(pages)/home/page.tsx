85a1bf65fa31626fe5b980c0cf4264f7208644f6
Thiago Nunes <desenvolvedor.nunes@gmail.com>
2023-09-19 16:35:21 -0300

    feat: adiciona listas de filmes na pagina

--- src/app/(pages)/home/page.tsx
+++ src/app/(pages)/home/page.tsx
@@ -1,3 +1,18 @@
-export default function HomePage() {
-  return <main className="right-0 w-[85%] bg-black"></main>
+import { Coursel } from '@app/components/carousel'
+import { MoviesList } from '@app/components/moviesList'
+
+export default async function HomePage() {
+  return (
+    <main className="right-0 w-[85%] bg-black">
+      <Coursel />
+
+      <MoviesList listTitle="Ação" genreId={28} />
+      <MoviesList listTitle="Aventura" genreId={12} />
+      <MoviesList listTitle="Animação" genreId={16} />
+      <MoviesList listTitle="Comédia" genreId={35} />
+      <MoviesList listTitle="Crime" genreId={80} />
+      <MoviesList listTitle="Documentário" genreId={99} />
+      <MoviesList listTitle="Drama" genreId={18} />
+    </main>
+  )
 }
