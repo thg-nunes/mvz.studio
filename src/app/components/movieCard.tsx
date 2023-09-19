2d0caf87bcb8cd9af95aac943f52d2361982528a
Thiago Nunes <desenvolvedor.nunes@gmail.com>
2023-09-19 16:34:16 -0300

    refactor: diminui tamanho da descricao do filme e trataiva para quando nao houver uma descricao

--- src/app/components/movieCard.tsx
+++ src/app/components/movieCard.tsx
@@ -12,40 +12,43 @@ export type MovieCardProps = {
 }
 
 export const MovieCard = ({
-  movieTitle,
-  movieDescription,
+  movieTitle = '',
+  movieDescription = '',
   moviePathImage,
   voteAvarege,
 }: MovieCardProps): JSX.Element => {
-  const movieDescriptionUpdated = movieDescription.slice(0, 40) + '...'
+  const movieTitleUpdated = movieTitle.slice(0, 14) + '...'
+
+  const movieDescriptionUpdated = movieDescription.length
+    ? movieDescription.slice(0, 36) + '...'
+    : 'este filme não tem descrição'
+
   return (
-    <>
-      <div className="relative h-60 max-w-[180px] overflow-hidden rounded-xl">
-        <Image
-          width={180}
-          height={180}
-          src={returnsMovieURL(500, moviePathImage)}
-          alt={`imagem de capa do filme ${movieTitle}`}
-        />
-        <button className="absolute right-2 top-2 rounded-sm bg-gray-500 p-1 text-xs text-white">
-          <AiOutlinePlus />
-        </button>
-        <section className="absolute bottom-0 left-0 flex h-max flex-col gap-2 bg-white px-1 py-2">
-          <div>
-            <p className="text-base font-bold">{movieTitle}</p>
-            <p className="text-xs font-semibold opacity-80">{movieDescriptionUpdated}</p>
-          </div>
-          <div className="flex items-center justify-between">
-            <span className="flex items-center text-xs font-semibold">
-              {voteAvarege}
-              <FaStar className="text-[10px] text-yellow-500" />
-            </span>
-            <button className="rounded-xl bg-orange-700 p-1 px-2 text-xs font-semibold text-white">
-              assistir
-            </button>
-          </div>
-        </section>
-      </div>
-    </>
+    <div className="relative h-60 max-w-[180px] overflow-hidden rounded-xl">
+      <Image
+        width={180}
+        height={180}
+        src={returnsMovieURL(500, moviePathImage)}
+        alt={`imagem de capa do filme ${movieTitle}`}
+      />
+      <button className="absolute right-2 top-2 rounded-sm bg-gray-500 p-1 text-xs text-white">
+        <AiOutlinePlus />
+      </button>
+      <section className="absolute bottom-0 left-0 flex h-max w-full flex-col gap-2 bg-white px-1 py-2">
+        <div>
+          <p className="text-base font-bold">{movieTitleUpdated}</p>
+          <p className="text-xs font-semibold opacity-80">{movieDescriptionUpdated}</p>
+        </div>
+        <div className="flex items-center justify-between">
+          <span className="flex items-center text-xs font-semibold">
+            {voteAvarege}
+            <FaStar className="text-[10px] text-yellow-500" />
+          </span>
+          <button className="rounded-xl bg-orange-700 p-1 px-2 text-xs font-semibold text-white">
+            assistir
+          </button>
+        </div>
+      </section>
+    </div>
   )
 }
