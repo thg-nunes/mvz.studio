d691f17b9f22be6436816aace458becd9a56a2e4
Thiago Nunes <desenvolvedor.nunes@gmail.com>
2023-09-21 14:55:39 -0300

    feat: cria utilitarios para obter os detalhes do filme e a key de video no youtube

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,10 +1,12 @@
 import { fetchData } from './fetch-data-config'
 
-import { CarouselMoviePropsDTO, MovieDTO } from '@dtos/movie'
+import { CarouselMoviePropsDTO, MovieDTO, MovieDetailsDTO } from '@dtos/movie'
 
 type ApiService = {
   fetchCarouselMovies(): Promise<CarouselMoviePropsDTO[]>
   fetchMoviesByGenre(data: { genreID: number; page: number }): Promise<MovieDTO[]>
+  fetchMovieDetailsById(movieId: string): Promise<MovieDetailsDTO>
+  fetchMovieVideoById(movieId: string): Promise<{ results: { key: string }[] }>
 }
 
 const apiService: ApiService = {
@@ -46,6 +48,29 @@ const apiService: ApiService = {
     )
 
     return results
+  },
+  fetchMovieDetailsById: async function (movieId: string): Promise<MovieDetailsDTO> {
+    const movieDetails = await fetchData<MovieDetailsDTO>(`/movie/${movieId}`, {
+      next: {
+        revalidate: 60 * 60 * 24 * 3, // 3 days
+      },
+    })
+
+    return movieDetails
+  },
+  fetchMovieVideoById: async function (
+    movieId: string
+  ): Promise<{ results: { key: string }[] }> {
+    const movieDetails = await fetchData<{ results: { key: string }[] }>(
+      `/movie/${movieId}/videos`,
+      {
+        next: {
+          revalidate: 60 * 60 * 24 * 3, // 3 days
+        },
+      }
+    )
+
+    return movieDetails
   },
 }
 
