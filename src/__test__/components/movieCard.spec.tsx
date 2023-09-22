7e481ef4c14e98b4c37373f3dac1a1a31968af5b
Thiago Nunes <desenvolvedor.nunes@gmail.com>
2023-09-22 13:05:00 -0300

    test: adiciona teste para caso de filme que nao tenha descricao

--- src/__test__/components/movieCard.spec.tsx
+++ src/__test__/components/movieCard.spec.tsx
@@ -4,12 +4,15 @@ import { MovieCard } from '@app/components/movieCard'
 
 describe('<MovieCard />', () => {
   it('Should render the movie card with image, vote, title and description', () => {
+    const any_movie_id = 112
     const movieTitle = 'any movie title'
     const voteAvarege = 7.5
     const moviePathImage = '/any-moviePathImage'
     const movieDescription = 'any movie movieDescription'
+
     render(
       <MovieCard
+        movie_id={any_movie_id}
         movieTitle={movieTitle}
         voteAvarege={voteAvarege}
         moviePathImage={moviePathImage}
@@ -21,8 +24,34 @@ describe('<MovieCard />', () => {
     expect(movieImage.tagName).toBe('IMG')
     expect(movieImage).toHaveProperty('alt', `imagem de capa do filme ${movieTitle}`)
 
-    expect(screen.getByText(movieTitle)).toBeInTheDocument()
+    expect(screen.getByText(movieTitle.slice(0, 14) + '...')).toBeInTheDocument()
     expect(screen.getByText(voteAvarege)).toBeInTheDocument()
-    expect(screen.getByText(movieDescription)).toBeInTheDocument()
+    expect(screen.getByText(movieDescription.slice(0, 36) + '...')).toBeInTheDocument()
+  })
+
+  it('Ensures that the card render an alternative text if the movie does not have a text description', () => {
+    const any_movie_id = 112
+    const movieTitle = 'any movie title'
+    const voteAvarege = 7.5
+    const moviePathImage = '/any-moviePathImage'
+    const movieDescription = ''
+
+    render(
+      <MovieCard
+        movie_id={any_movie_id}
+        movieTitle={movieTitle}
+        voteAvarege={voteAvarege}
+        moviePathImage={moviePathImage}
+        movieDescription={movieDescription}
+      />
+    )
+    const movieImage = screen.getByAltText(`imagem de capa do filme ${movieTitle}`)
+
+    expect(movieImage.tagName).toBe('IMG')
+    expect(movieImage).toHaveProperty('alt', `imagem de capa do filme ${movieTitle}`)
+
+    expect(screen.getByText(movieTitle.slice(0, 14) + '...')).toBeInTheDocument()
+    expect(screen.getByText(voteAvarege)).toBeInTheDocument()
+    expect(screen.getByText('este filme não tem descrição')).toBeInTheDocument()
   })
 })
