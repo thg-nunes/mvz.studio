b9fa6fffc7ecc0baaca7dff506718a6131c2674b
Thiago Nunes <desenvolvedor.nunes@gmail.com>
2023-09-23 11:45:43 -0300

    test: arante que a pagina de detalhes do filme tenha um link para voltar para a pagina inicial

---
+++ src/__test__/pages/detalhes/layout.spec.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react'
+import MovieDetailsLayout from '@app/(pages)/detalhes/[movie_id]/layout'
+
+describe('<MovieDetailsLayout />', () => {
+  it('Ensures that in movie details page has a link to back to home page', () => {
+    render(
+      <MovieDetailsLayout>
+        <p>empty</p>
+      </MovieDetailsLayout>
+    )
+
+    expect(
+      screen.getByText((content, element) => {
+        return (
+          element?.tagName.toLowerCase() === 'a' &&
+          element.getAttribute('href') === '/home'
+        )
+      })
+    ).toBeInTheDocument()
+  })
+})
