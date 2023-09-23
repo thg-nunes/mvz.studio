35e7107df6b1c88826864833590d9c479968536d
Thiago Nunes <desenvolvedor.nunes@gmail.com>
2023-09-23 10:35:46 -0300

    test: garante que o gancho useFetchMoviesByGenre obtenha e retorne a lista de filmes corretamente

---
+++ src/__test__/hooks/moviesList.spec.ts
@@ -0,0 +1,149 @@
+import { useState, useEffect } from 'react'
+import { waitFor } from '@testing-library/react'
+
+import { apiService } from '@app/services/api'
+import { useFetchMoviesByGenre } from '@app/hooks/components/moviesList'
+
+jest.mock('react', () => {
+  return {
+    ...jest.requireActual('react'),
+    useEffect: jest.fn((callback) => callback()),
+    useState: jest.fn(),
+  }
+})
+
+jest.mock('@app/services/api', () => {
+  return {
+    apiService: {
+      fetchMoviesByGenre: jest.fn(),
+    },
+  }
+})
+
+const moviesList = [
+  {
+    adult: false,
+    backdrop_path: '/55Rb9qt3yzyF4KQpC1c3T3Fbcao.jpg',
+    genre_ids: [27, 53],
+    id: 1008042,
+    original_language: 'en',
+    original_title: 'Talk to Me',
+    overview:
+      'When a group of friends discover how to conjure spirits using an embalmed hand, they become hooked on the new thrill, until one of them goes too far and unleashes terrifying supernatural forces.',
+    popularity: 3538.457,
+    poster_path: '/kdPMUMJzyYAc4roD52qavX0nLIC.jpg',
+    release_date: '2023-07-26',
+    title: 'Talk to Me',
+    video: false,
+    vote_average: 7.3,
+    vote_count: 613,
+  },
+  {
+    adult: false,
+    backdrop_path: '/ctMserH8g2SeOAnCw5gFjdQF8mo.jpg',
+    genre_ids: [35, 12, 14],
+    id: 346698,
+    original_language: 'en',
+    original_title: 'Barbie',
+    overview:
+      'Barbie and Ken are having the time of their lives in the colorful and seemingly perfect world of Barbie Land. However, when they get a chance to go to the real world, they soon discover the joys and perils of living among humans.',
+    popularity: 2820.205,
+    poster_path: '/iuFNMS8U5cb6xfzi51Dbkovj7vM.jpg',
+    release_date: '2023-07-19',
+    title: 'Barbie',
+    video: false,
+    vote_average: 7.3,
+    vote_count: 4668,
+  },
+  {
+    adult: false,
+    backdrop_path: '/4XM8DUTQb3lhLemJC51Jx4a2EuA.jpg',
+    genre_ids: [28, 80, 53],
+    id: 385687,
+    original_language: 'en',
+    original_title: 'Fast X',
+    overview:
+      "Over many missions and against impossible odds, Dom Toretto and his family have outsmarted, out-nerved and outdriven every foe in their path. Now, they confront the most lethal opponent they've ever faced: A terrifying threat emerging from the shadows of the past who's fueled by blood revenge, and who is determined to shatter this family and destroy everything—and everyone—that Dom loves, forever.",
+    popularity: 2774.925,
+    poster_path: '/fiVW06jE7z9YnO4trhaMEdclSiC.jpg',
+    release_date: '2023-05-17',
+    title: 'Fast X',
+    video: false,
+    vote_average: 7.3,
+    vote_count: 3736,
+  },
+  {
+    adult: false,
+    backdrop_path: '/8pjWz2lt29KyVGoq1mXYu6Br7dE.jpg',
+    genre_ids: [28, 878, 27],
+    id: 615656,
+    original_language: 'en',
+    original_title: 'Meg 2: The Trench',
+    overview:
+      'An exploratory dive into the deepest depths of the ocean of a daring research team spirals into chaos when a malevolent mining operation threatens their mission and forces them into a high-stakes battle for survival.',
+    popularity: 2429.447,
+    poster_path: '/4m1Au3YkjqsxF8iwQy0fPYSxE0h.jpg',
+    release_date: '2023-08-02',
+    title: 'Meg 2: The Trench',
+    video: false,
+    vote_average: 7,
+    vote_count: 1790,
+  },
+  {
+    adult: false,
+    backdrop_path: '/53z2fXEKfnNg2uSOPss2unPBGX1.jpg',
+    genre_ids: [27, 9648, 53],
+    id: 968051,
+    original_language: 'en',
+    original_title: 'The Nun II',
+    overview:
+      'In 1956 France, a priest is violently murdered, and Sister Irene begins to investigate. She once again comes face-to-face with a powerful evil.',
+    popularity: 1849.236,
+    poster_path: '/5gzzkR7y3hnY8AD1wXjCnVlHba5.jpg',
+    release_date: '2023-09-06',
+    title: 'The Nun II',
+    video: false,
+    vote_average: 6.6,
+    vote_count: 211,
+  },
+  {
+    adult: false,
+    backdrop_path: '/c6Splshb8lb2Q9OvUfhpqXl7uP0.jpg',
+    genre_ids: [28, 53],
+    id: 717930,
+    original_language: 'en',
+    original_title: 'Kandahar',
+    overview:
+      'After his mission is exposed, an undercover CIA operative stuck deep in hostile territory in Afghanistan must fight his way out, alongside his Afghan translator, to an extraction point in Kandahar, all whilst avoiding elite enemy forces and foreign spies tasked with hunting them down.',
+    popularity: 1824.055,
+    poster_path: '/lCanGgsqF4xD2WA5NF8PWeT3IXd.jpg',
+    release_date: '2023-05-25',
+    title: 'Kandahar',
+    video: false,
+    vote_average: 6.8,
+    vote_count: 498,
+  },
+]
+
+describe('useFetchMoviesByGenre', () => {
+  const movieList: unknown[] = []
+  const setMovieList = jest.fn()
+
+  beforeEach(() => {
+    jest.mocked(useEffect).mockImplementation((callback) => callback())
+
+    jest.mocked(useState).mockReturnValue([movieList, setMovieList])
+  })
+
+  it('Ensures that the hook useFetchMoviesByGenre get and returns the movie list correctly', async () => {
+    const fakeMovieGenre = 123
+    const page = 10
+
+    jest.mocked(apiService.fetchMoviesByGenre).mockResolvedValueOnce(moviesList)
+
+    const response = useFetchMoviesByGenre(fakeMovieGenre, page)
+
+    await waitFor(() => expect(setMovieList).toHaveBeenCalledWith(moviesList))
+    await waitFor(() => expect(response).toEqual(movieList))
+  })
+})
