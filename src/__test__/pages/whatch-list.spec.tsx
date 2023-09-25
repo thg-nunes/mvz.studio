297f27ec3ad86a96b4a817d3a942dc45a96f687b
Thiago Nunes <desenvolvedor.nunes@gmail.com>
2023-09-25 15:06:07 -0300

    test: garante que a WhatchListPage renderize os elementos corretos

---
+++ src/__test__/pages/whatch-list.spec.tsx
@@ -0,0 +1,45 @@
+import { render, screen } from '@testing-library/react'
+
+import { useGetMoviesById } from '@app/hooks/pages/whatch-list'
+
+import WhatchListPage from '@app/(pages)/whatch-list/page'
+
+jest.mock('@app/hooks/pages/whatch-list')
+
+describe('<WhatchListPage />', () => {
+  const fakeMoviesResponse = {
+    adult: false,
+    backdrop_path: '/55Rb9qt3yzyF4KQpC1c3T3Fbcao.jpg',
+    genres: [
+      {
+        id: 1,
+        name: 'any_genre',
+      },
+    ],
+    production_companies: [{ name: 'any_companie_name' }],
+    runtime: 159,
+    id: 1008042,
+    original_language: 'en',
+    original_title: 'Talk to Me',
+    overview:
+      'When a group of friends discover how to conjure spirits using an embalmed hand, they become hooked on the new thrill, until one of them goes too far and unleashes terrifying supernatural forces.',
+    popularity: 3538.457,
+    poster_path: '/kdPMUMJzyYAc4roD52qavX0nLIC.jpg',
+    release_date: '2023-07-26',
+    title: 'Talk to Me',
+    video: false,
+    vote_average: 7.3,
+    vote_count: 613,
+  }
+
+  it('Ensures that the whatch-list page render the correct elements', () => {
+    jest.mocked(useGetMoviesById).mockReturnValue([fakeMoviesResponse])
+
+    render(<WhatchListPage />)
+
+    expect(screen.getByText(`Título: ${fakeMoviesResponse.title}`)).toBeInTheDocument()
+    expect(screen.getByText(fakeMoviesResponse.overview)).toBeInTheDocument()
+    expect(screen.getByText(fakeMoviesResponse.release_date)).toBeInTheDocument()
+    expect(screen.getByText(`${fakeMoviesResponse.runtime} min`)).toBeInTheDocument()
+  })
+})
