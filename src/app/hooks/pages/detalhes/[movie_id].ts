70156e5778f0ecfae4bc8524701fdfb53e3cc514
thiago nunes <desenvolvedor.nunes@gmail.com>
2023-09-29 16:09:57 -0300

    refactor: muda tratativa caso o filme nao seja encontrado na api do tmdb

--- src/app/hooks/pages/detalhes/[movie_id].ts
+++ src/app/hooks/pages/detalhes/[movie_id].ts
@@ -1,10 +1,17 @@
-import { apiService } from '@app/services/api'
+import { notFound } from 'next/navigation'
+
 import { MovieDetailsDTO } from '@dtos/movie'
+import { apiService } from '@app/services/api'
 
 export const useFetchMovieDetails = async (
   movie_id: string
 ): Promise<{ movieDetails: MovieDetailsDTO; movieVideoKey: string }> => {
   const { results } = await apiService.fetchMovieVideoById(movie_id)
+
+  if (!results || !results.length) {
+    notFound()
+  }
+
   const movieVideoKey = results[0].key
 
   const movieDetails = await apiService.fetchMovieDetailsById(movie_id)
