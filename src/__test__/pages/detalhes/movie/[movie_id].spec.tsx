65824f4c4cddf3441e4923ef6785ce8f52518739
thiago nunes <desenvolvedor.nunes@gmail.com>
2023-09-29 15:30:16 -0300

    test: garante que a funcao metadata retorne o titulo correto da pagina

--- src/__test__/pages/detalhes/movie/[movie_id].spec.tsx
+++ src/__test__/pages/detalhes/movie/[movie_id].spec.tsx
@@ -1,7 +1,9 @@
 import { render, screen, waitFor } from '@testing-library/react'
 
 import { apiService } from '@app/services/api'
-import MovieDetails from '@app/(pages)/detalhes/[movie_id]/page'
+import MovieDetails, {
+  generateMetadata,
+} from '@app/(pages)/detalhes/movie/[movie_id]/page'
 import { useFetchMovieDetails } from '@app/hooks/pages/detalhes/[movie_id]'
 
 jest.mock('@app/services/api')
@@ -138,5 +140,43 @@ describe('<MovieDetails />', () => {
     await waitFor(() =>
       expect(screen.getByText('Classificação livre')).toBeInTheDocument()
     )
+  })
+
+  it('Ensures that the metadata function returns the correct title of  page', async () => {
+    const release_date = '31-08-2023'
+    const popularity = 6.951
+    const vote_average = 152.04
+
+    jest.mocked(useFetchMovieDetails).mockResolvedValue({
+      movieVideoKey: 'movie_video_key',
+      movieDetails: {
+        adult: false,
+        backdrop_path: '/backdrop_path',
+        genres: [
+          {
+            id: 12,
+            name: 'any_genre',
+          },
+        ],
+        id: 123,
+        original_language: 'any_language',
+        original_title: 'any_title',
+        overview: 'any_overview',
+        popularity,
+        poster_path: '/any_poster_path',
+        production_companies: [{ name: 'any_companie' }],
+        release_date,
+        runtime: 159,
+        title: 'any_title',
+        video: false,
+        vote_average,
+        vote_count: 1520,
+      },
+    })
+    const { title } = await generateMetadata({ params: { movie_id: '123' } })
+
+    await waitFor(() => expect(useFetchMovieDetails).toHaveBeenCalledWith('123'))
+
+    await waitFor(() => expect(title).toEqual('MVZ.studio | Filme: any_title'))
   })
 })
