641470240531154a4d375b8e8547e07e88c64614
thiago nunes <desenvolvedor.nunes@gmail.com>
2023-09-30 10:41:21 -0300

    chore: adiciona type para o objeto de series de tv

--- src/dtos/movie.ts
+++ src/dtos/movie.ts
@@ -42,3 +42,61 @@ export type TopRatedTVShowsPropsDTO = Omit<
 }
 
 export type ListTypeDTO = 'tv' | 'movie'
+
+export type TVSerieDetails = Pick<
+  MovieDTO,
+  'adult' | 'overview' | 'backdrop_path' | 'id'
+> & {
+  episode_run_time: number[]
+  first_air_date: string
+  created_by: [
+    {
+      name: string
+      profile_path: string
+    },
+  ]
+  genres: {
+    id: number
+    name: string
+  }[]
+  in_production: boolean
+  last_air_date: string
+  last_episode_to_air: {
+    id: number
+    name: string
+    air_date: Date
+    episode_number: number
+    episode_type: string
+    runtime: number
+    season_number: number
+    show_id: number
+  }
+  name: string
+  next_episode_to_air: {
+    id: number
+    name: string
+    overview: ''
+    air_date: '2023-09-29'
+    episode_number: number
+    episode_type: string
+    runtime: number
+    season_number: number
+    show_id: number
+  }
+  number_of_episodes: number
+  number_of_seasons: number
+  popularity: number
+  seasons: {
+    air_date: Date
+    episode_count: number
+    id: number
+    name: string
+    overview: string
+    poster_path: string
+    season_number: number
+    vote_average: number
+  }[]
+  type: string
+  vote_average: number
+  vote_count: number
+}
