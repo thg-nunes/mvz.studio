1c68f11e68f63934ec012ee0da350ecf31e1e31e
thiago nunes <desenvolvedor.nunes@gmail.com>
2023-10-01 10:29:33 -0300

    feat: iniciando exibicao das 10 imagens da serie

--- src/app/(pages)/detalhes/tv/[tv_serie_id]/page.tsx
+++ src/app/(pages)/detalhes/tv/[tv_serie_id]/page.tsx
@@ -1,44 +1,44 @@
-import { returnsMovieImageURL } from '@utils/movieImage'
+import Link from 'next/link'
+import { AiFillStar } from 'react-icons/ai'
+
+import { returnsMovieImageURL, returnsTVSerieImageURL } from '@utils/movieImage'
 import { useFetchTVSerieDetails } from '@app/hooks/pages/detalhes/tv/serie_id'
 
 import { SerieDetails } from './serieDetails'
-import { AiFillStar } from 'react-icons/ai'
-import Link from 'next/link'
 
 export default async function TvSerieDetailsById({
   params,
 }: {
   params: { tv_serie_id: string }
 }) {
-  const response = await useFetchTVSerieDetails(params.tv_serie_id)
-  const lastSeason = response.seasons.pop()
+  const { serieDetails, serieImages } = await useFetchTVSerieDetails(params.tv_serie_id)
+  const lastSeason = serieDetails.seasons.pop()
   const numberOfLastSeason = lastSeason ? lastSeason.season_number + 1 : 1
-  const lastAirDate = response.last_air_date.split('/').pop()
+  const lastAirDate = serieDetails.last_air_date.split('/').pop()
 
   return (
     <div className="">
       <div className="relative h-[450px]">
         <img
-          src={returnsMovieImageURL(500, response.backdrop_path)}
-          alt={`imagem do filme ${response.name}`}
+          src={returnsMovieImageURL(500, serieDetails.backdrop_path)}
+          alt={`imagem do filme ${serieDetails.name}`}
           className="carouselElement h-full w-full object-cover"
         />
         <section className="absolute left-0 top-0 flex h-full w-full items-center gap-4 bg-black/70 px-3">
           <img
-            src={returnsMovieImageURL(500, response.backdrop_path)}
-            alt={`imagem do filme ${response.name}`}
+            src={returnsMovieImageURL(500, serieDetails.backdrop_path)}
+            alt={`imagem do filme ${serieDetails.name}`}
             className="carouselElement h-3/4 w-1/4 rounded-lg object-cover"
           />
-          <SerieDetails serieData={response} />
+          <SerieDetails serieData={serieDetails} />
         </section>
       </div>
-
       <div className="mx-auto w-[85%] py-6">
         <p className="mb-3 text-2xl font-semibold text-white">Temporada Atual</p>
         <section className="mb-4 flex gap-3 overflow-hidden rounded-lg bg-gray-900 text-white shadow-3xl shadow-white/20">
           <img
-            src={returnsMovieImageURL(500, response.backdrop_path)}
-            alt={`imagem do filme ${response.name}`}
+            src={returnsMovieImageURL(500, serieDetails.backdrop_path)}
+            alt={`imagem do filme ${serieDetails.name}`}
             className="carouselElement h-[192px] w-[144px] object-cover"
           />
           <div className="flex max-h-[192px] flex-col justify-around">
@@ -50,7 +50,7 @@ export default async function TvSerieDetailsById({
                   <AiFillStar className="text-xs" /> {lastSeason?.vote_average}
                 </p>
                 <p className="text-sm font-semibold">
-                  {lastAirDate} • {response.number_of_episodes} episódios
+                  {lastAirDate} • {serieDetails.number_of_episodes} episódios
                 </p>
               </section>
             </section>
@@ -61,16 +61,40 @@ export default async function TvSerieDetailsById({
             </section>
 
             <p className="text-sm">
-              NOTA: A {numberOfLastSeason}ª temporada de {response.name} começou a ser
-              exibida em {response.last_air_date}.
+              NOTA: A {numberOfLastSeason}ª temporada de {serieDetails.name} começou a ser
+              exibida em {serieDetails.last_air_date}.
             </p>
           </div>
         </section>
 
+        {/* LINK DA PAGINA DE MAIS TEMPORADAS, SEGUIR O MODELO: https://www.themoviedb.org/tv/1416-grey-s-anatomy/seasons?language=pt-BR */}
         <Link href="/temporadas" className="text-white duration-150 hover:text-gray-400">
           mostrar mais temporadas
         </Link>
       </div>
+
+      {/* ADICIONAR AS INFORMAÇÕES GLOBAIS DA SERIE */}
+      <div></div>
+
+      {/* FAZER A GALERIA DE IMAGENS DA SERIE */}
+      <div className="mx-auto w-[85%] ">
+        <p className="font-semibold">Imagens da série</p>
+        <section className="flex overflow-x-scroll">
+          {!!serieImages.length &&
+            serieImages.slice(0, 11).map((image) => {
+              return (
+                <img
+                  key={image.file_path}
+                  alt="imagem de uma cena da série"
+                  src={returnsMovieImageURL(500, image.file_path)}
+                />
+              )
+            })}
+        </section>
+      </div>
+
+      {/* ADICIONA UMA LISTA DE RECOMENDAÇÕES */}
+      <div></div>
     </div>
   )
 }
--- src/app/hooks/pages/detalhes/tv/serie_id.ts
+++ src/app/hooks/pages/detalhes/tv/serie_id.ts
@@ -1,20 +1,21 @@
 import { notFound } from 'next/navigation'
 
 import { apiService } from '@app/services/api'
-import { TVSerieDetails } from '@dtos/movie'
+import { TVSerieDetails, TVSerieImaesDTO } from '@dtos/movie'
 
 export const useFetchTVSerieDetails = async (
   tv_serie_id: string
-): Promise<TVSerieDetails> => {
-  const response = await apiService.fetchTvShowDetails(tv_serie_id)
+): Promise<{ serieDetails: TVSerieDetails; serieImages: TVSerieImaesDTO }> => {
+  const serieDetails = await apiService.fetchTvShowDetails(tv_serie_id)
+  const { backdrops: serieImages } = await apiService.fetchTvShowImages(tv_serie_id)
 
-  if (!response) {
+  if (!serieDetails) {
     notFound()
   }
 
-  response.last_air_date = new Date(response.last_air_date).toLocaleDateString()
-  response.vote_count = parseInt(response.vote_count.toFixed(0))
-  response.vote_average = parseInt(response.vote_average.toFixed(0)) * 10
+  serieDetails.last_air_date = new Date(serieDetails.last_air_date).toLocaleDateString()
+  serieDetails.vote_count = parseInt(serieDetails.vote_count.toFixed(0))
+  serieDetails.vote_average = parseInt(serieDetails.vote_average.toFixed(0)) * 10
 
-  return response
+  return { serieDetails, serieImages }
 }
