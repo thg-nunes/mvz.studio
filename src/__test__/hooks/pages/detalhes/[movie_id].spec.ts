0f7e1171d6e6e2e297bea2a23fa9243e6e908980
thiago nunes <desenvolvedor.nunes@gmail.com>
2023-10-06 12:47:09 -0300

    test: garante que a funcao notFound seja chamada ses o resulttado da busca for nulo ou nao tiver conteudo

--- src/__test__/hooks/pages/detalhes/[movie_id].spec.ts
+++ src/__test__/hooks/pages/detalhes/[movie_id].spec.ts
@@ -1,6 +1,10 @@
+import { notFound } from 'next/navigation'
+
 import { apiService } from '@app/services/api'
 import { useFetchMovieDetails } from '@app/hooks/pages/detalhes/[movie_id]'
+import { waitFor } from '@testing-library/react'
 
+jest.mock('next/navigation')
 jest.mock('@app/services/api')
 
 describe('useFetchMovieDetails', () => {
@@ -51,5 +55,43 @@ describe('useFetchMovieDetails', () => {
       parseInt(movieDetails.vote_average.toFixed(0))
     )
     expect(movieVideoKey).toEqual('any_movie_video_key')
+  })
+
+  it('Ensures that the notFound function is called if the result of fetch is null or not contains content', async () => {
+    const release_date = '31-08-2023'
+    const popularity = 6.951
+    const vote_average = 152.04
+
+    jest.mocked(apiService.fetchMovieVideoById).mockResolvedValue({
+      results: [],
+    })
+
+    jest.mocked(apiService.fetchMovieDetailsById).mockResolvedValueOnce({
+      adult: true,
+      backdrop_path: '/backdrop_path',
+      genres: [
+        {
+          id: 12,
+          name: 'any_genre',
+        },
+      ],
+      id: 123,
+      original_language: 'any_language',
+      original_title: 'any_title',
+      overview: 'any_overview',
+      popularity,
+      poster_path: '/any_poster_path',
+      production_companies: [{ name: 'any_companie' }],
+      release_date,
+      runtime: 159,
+      title: 'any_title',
+      video: false,
+      vote_average,
+      vote_count: 1520,
+    })
+
+    await useFetchMovieDetails('123')
+
+    await waitFor(() => expect(notFound).toHaveBeenCalled())
   })
 })
