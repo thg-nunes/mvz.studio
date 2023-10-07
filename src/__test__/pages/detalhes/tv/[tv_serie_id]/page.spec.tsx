6998d69b9e2b5571d4d95e48ab62da29bff92f2c
thiago nunes <desenvolvedor.nunes@gmail.com>
2023-10-07 14:20:37 -0300

    test: garante que o caso para renderizar o genero da serie execute corretamente

--- src/__test__/pages/detalhes/tv/[tv_serie_id]/page.spec.tsx
+++ src/__test__/pages/detalhes/tv/[tv_serie_id]/page.spec.tsx
@@ -8,7 +8,10 @@ jest.mock('@utils/movieOnWhatchList')
 describe('<SerieDetails />', () => {
   const serie_details = {
     name: 'fake_name',
-    genres: [{ id: 1, name: 'any_genre' }],
+    genres: [
+      { id: 1, name: 'any_genre1' },
+      { id: 2, name: 'any_genre2' },
+    ],
     vote_average: 1523,
     id: 12548,
     overview: 'fake_overview',
@@ -32,5 +35,30 @@ describe('<SerieDetails />', () => {
     act(() => fireEvent.click(screen.getByText('+whatch-list')))
 
     expect(addMovieOnWhatchList).toHaveBeenCalledWith(serie_details.id)
+  })
+
+  it('ensures that the case to render serie genre execute correctly', () => {
+    render(
+      <SerieDetails
+        serieData={{
+          ...serie_details,
+          overview: '',
+          created_by: [
+            {
+              name: 'any_name',
+              profile_path: 'fake_profile_path',
+            },
+          ],
+        }}
+      />
+    )
+
+    expect(
+      screen.getByText('Este filme não possui um overview escrito.')
+    ).toBeInTheDocument()
+
+    expect(screen.getByText('any_genre1 |')).toBeInTheDocument()
+
+    expect(screen.getByText('any_genre2')).toBeInTheDocument()
   })
 })
