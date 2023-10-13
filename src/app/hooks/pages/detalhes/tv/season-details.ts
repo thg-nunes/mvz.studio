5028c50fe15b5bc1dff1f8b2f2d95010b2db1aa6
thiago nunes <desenvolvedor.nunes@gmail.com>
2023-10-13 15:14:11 -0300

    chore: tratando os dados vindos da api para o formato desejado

--- src/app/hooks/pages/detalhes/tv/season-details.ts
+++ src/app/hooks/pages/detalhes/tv/season-details.ts
@@ -1,18 +1,38 @@
 import { notFound } from 'next/navigation'
 
-import { TVSeasonDetailsDTO } from '@dtos/movie'
+import { TVSeasonDetailsDTO, TVSerieImaesDTO } from '@dtos/movie'
 
 import { apiService } from '@app/services/api'
 import { SeasonDetailsPageParams } from '@app/(pages)/detalhes/tv/[tv_serie_id]/season/[season_number]/page'
+import { returnsDateInWriting } from '@utils/date-formate'
 
 export const useFetchTVSeasonDetails = async (
   data: SeasonDetailsPageParams
-): Promise<TVSeasonDetailsDTO> => {
+): Promise<{ seasonDetails: TVSeasonDetailsDTO; serieImages: TVSerieImaesDTO }> => {
   const seasonDetails = await apiService.fetchTVSeasonDetails(data)
+  const { backdrops: serieImages } = await apiService.fetchTvShowImages(
+    data.params.tv_serie_id
+  )
 
   if (!seasonDetails.air_date) {
     notFound()
   }
 
-  return seasonDetails
+  seasonDetails.vote_average = parseFloat(seasonDetails.vote_average.toFixed(1))
+
+  seasonDetails.episodes = seasonDetails.episodes.map((ep) => {
+    const [year, month, day] = ep.air_date.split('-')
+    const _year = parseInt(year)
+    const _month = parseInt(month)
+    const _day = parseInt(day)
+
+    ep.overview = ep.overview ? ep.overview : 'Este ep não contém um overview formado.'
+
+    return {
+      ...ep,
+      air_date: returnsDateInWriting(_year, _month, _day),
+    }
+  })
+
+  return { seasonDetails, serieImages }
 }
