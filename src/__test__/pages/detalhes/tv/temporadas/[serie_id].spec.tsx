421328ed81e85a7caa11c04c94c04f00e01f2198
thiago nunes <desenvolvedor.nunes@gmail.com>
2023-10-20 11:33:07 -0300

    test: continuacao dos testes para a pagina de detalhes de uma temporada de serie de tv

--- src/__test__/pages/detalhes/tv/temporadas/[serie_id].spec.tsx
+++ src/__test__/pages/detalhes/tv/temporadas/[serie_id].spec.tsx
@@ -1,8 +1,12 @@
-import { render, waitFor } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 
 import { useFetchSerieSeason } from '@app/hooks/pages/temporadas/[tv_serie_id]'
-import TVSerieSeasons from '@app/(pages)/detalhes/tv/temporadas/[serie_id]/page'
+import TVSerieSeasons, {
+  generateMetadata,
+} from '@app/(pages)/detalhes/tv/temporadas/[serie_id]/page'
+import { useFetchTVSerieDetails } from '@app/hooks/pages/detalhes/tv/serie_id'
 
+jest.mock('@app/hooks/pages/detalhes/tv/serie_id')
 jest.mock('@app/hooks/pages/temporadas/[tv_serie_id]')
 
 describe('<TVSerieSeasons />', () => {
@@ -19,7 +23,20 @@ describe('<TVSerieSeasons />', () => {
       season_number: 1,
       vote_average: 8.8,
     }
+    const fake_season_name = 'fake_serie_name'
 
+    jest.mocked(useFetchTVSerieDetails).mockResolvedValue({
+      serieDetails: {
+        name: fake_season_name,
+      },
+    } as any)
+
+    global.fetch = jest.fn().mockResolvedValue({
+      json: jest.fn().mockResolvedValue([fake_season_response]),
+    })
+
+    // CONTINUAR OS TESTES PARA AS PAGINAS DE: TVSerieSeasons
+    // CONTINUAR OS TESTES PARA OS HOOKS DE: useFetchTVSeasonDetails
     jest.mocked(useFetchSerieSeason).mockResolvedValueOnce([fake_season_response])
 
     render(await TVSerieSeasons({ params: fake_serie_season }))
@@ -27,5 +44,15 @@ describe('<TVSerieSeasons />', () => {
     await waitFor(() =>
       expect(useFetchSerieSeason).toHaveBeenCalledWith(fake_serie_season.serie_id)
     )
+
+    await waitFor(() =>
+      expect(
+        screen.getByAltText(`imagem do filme ${fake_season_response.name}`)
+      ).toBeInTheDocument()
+    )
+
+    const response = await generateMetadata({ params: fake_serie_season })
+
+    await waitFor(() => expect(response).toHaveProperty('title', '| ' + fake_season_name))
   })
 })
